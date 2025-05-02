5653b3b37cae8a1b47de98304b90df160a16a30a
sukhmani <sukhmani@bellevuecollege.edu>
2025-05-02 03:37:11 +0000

    updated

---
+++ string_operations.py
@@ -0,0 +1,4 @@
+message = "Python is amazing!"
+print(message.lower())  # Convert to lowercase
+print(message.find("is"))  # Find the position of a substring
+print(message.replace("amazing", "awesome"))  # Replace a word
